c38a6b9a799a12ebfc53bf6378202654cae5e6a4
vV-kimdohyun-Vv <kimdohyun2328@gmail.com>
2023-04-12 22:52:36 +0900

    Change 'textBox' class
    Added stunStr and several method in class 'textBox'

--- src/main.py
+++ src/main.py
@@ -4,22 +4,23 @@ from textbox import textBox
 from ime import IME
 
 DEL_THRES = 300
-#THRES_STUNNED = 1500  # 경직된 글자를 지우는 데 드는 시간.
-LATENCY_INIT = 100 # 백스페이스를 꾹 눌렀을 때 지워지는 사이 간격
+# THRES_STUNNED = 1500  # 경직된 글자를 지우는 데 드는 시간.
+LATENCY_INIT = 100  # 백스페이스를 꾹 눌렀을 때 지워지는 사이 간격
 LATENCY = LATENCY_INIT
 
 pygame.init()
 
-def game(): ##페이커는 엄마가 없다.
+
+def game():  ##페이커는 엄마가 없다.
 
     screen_width = 800
     screen_height = 500
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("TADAK")
 
-    myFont = pygame.font.SysFont("나눔고딕", 50)
-    myTextBox = textBox(myFont ,c=(255,255,255))
-    enemyTextBox = textBox(myFont, c=(255,255,255))
+    myFont = pygame.font.SysFont("Nanum Gothic", 50)
+    myTextBox = textBox(myFont)
+    enemyTextBox = textBox(myFont)
     enemyStr = ''
     enemyAtk = ['strike', 'bash', 'slash']
     currIdx = -1
@@ -48,7 +49,8 @@ def game(): ##페이커는 엄마가 없다.
     isKor = True
     shiftPressed = False
 
-    exceptChar = [pygame.K_BACKSPACE, pygame.K_RETURN, pygame.K_LSHIFT, pygame.K_RSHIFT, pygame.K_LALT, pygame.K_RALT] # 글자 아닌 키들 분류
+    exceptChar = [pygame.K_BACKSPACE, pygame.K_RETURN, pygame.K_LSHIFT, pygame.K_RSHIFT, pygame.K_LALT,
+                  pygame.K_RALT]  # 글자 아닌 키들 분류
 
     while running:
         '''값 입력'''
@@ -67,41 +69,46 @@ def game(): ##페이커는 엄마가 없다.
                 if event.key not in exceptChar:
                     try:
                         c = chr(event.key)
-                        if shiftPressed and (97<=ord(c)<=122):
+                        if shiftPressed and (97 <= ord(c) <= 122):
                             c = c.upper()
-                        keyTuple = gameIME.getKey(c,isKor)
-                        if(keyTuple[1]):
-                            try: myTextBox.addStr(keyTuple[0])
-                            except: pass
+                        keyTuple = gameIME.getKey(c, isKor)
+                        if (keyTuple[1]):
+                            try:
+                                myTextBox.addMainStr(keyTuple[0])
+                            except:
+                                pass
                         else:
                             try:
-                                myTextBox.subStr(1)
-                                myTextBox.addStr(keyTuple[0])
-                            except: pass
-                        
-                        if(len(keyTuple) == 4):
-                            if(keyTuple[3]):
-                                try: myTextBox.addStr(keyTuple[2])
-                                except: pass
+                                myTextBox.subMainStrFromRight(1)
+                                myTextBox.addMainStr(keyTuple[0])
+                            except:
+                                pass
+
+                        if (len(keyTuple) == 4):
+                            if (keyTuple[3]):
+                                try:
+                                    myTextBox.addMainStr(keyTuple[2])
+                                except:
+                                    pass
                             else:
                                 try:
-                                    myTextBox.subStr(1)
-                                    myTextBox.addStr(keyTuple[2])
-                                except: pass
-                    except: pass
-                
-                
+                                    myTextBox.subMainStrFromRight(1)
+                                    myTextBox.addMainStr(keyTuple[2])
+                                except:
+                                    pass
+                    except:
+                        pass
 
             if event.type == pygame.KEYUP:
                 if event.key in [pygame.K_LSHIFT, pygame.K_RSHIFT]:
                     shiftPressed = False
         '''벡스페이스'''
         if pygame.key.get_pressed()[pygame.K_BACKSPACE]:  # 벡스페이스 누르는 동안 bspc_t 증가
-            if backspace_t ==0: 
+            if backspace_t == 0:
                 bsp = gameIME.backSpace()
-                myTextBox.subStr(1)
+                myTextBox.subMainStrFromRight(1)
                 if bsp != None:
-                    myTextBox.addStr(bsp)
+                    myTextBox.addMainStr(bsp)
             backspace_t += 1
         else:
             backspace_t = 0  # 떼면 초기화
@@ -109,43 +116,41 @@ def game(): ##페이커는 엄마가 없다.
             LATENCY = LATENCY_INIT
         '''엔터'''
         if pygame.key.get_pressed()[pygame.K_RETURN]:
-            if myTextBox.getStr() != '':
-                parryStr = myTextBox.getStr()
-                if parryTime > 0 and enemyTextBox.getStr() == parryStr:
+            if myTextBox.getMainStr() != '':
+                parryStr = myTextBox.getMainStr()
+                if parryTime > 0 and enemyTextBox.getMainStr() == parryStr:
                     parrySuccess = True
                     parryTextTime = 1
                 if parryTime == 0: parryTime = 1
-                myTextBox.setStr('')
+                myTextBox.setMainStr('')
         '''쉬프트'''
         if pygame.key.get_pressed()[pygame.K_LSHIFT] or pygame.key.get_pressed()[pygame.K_RSHIFT]:
             shiftPressed = True
 
-        
-
-        if myTextBox.getLen() > 0:
+        if myTextBox.getMainStr() != '':
             if backspace_t - bspcOffset >= DEL_THRES:
-                myTextBox.subStr(1)
+                myTextBox.subMainStrFromRight(1)
                 bspcOffset += LATENCY
                 LATENCY = max(LATENCY_INIT // 2, LATENCY - 10)
 
         if not atkSet:
             currIdx = random.randint(0, len(enemyAtk) - 1)
             atkSet = True
-            if parryTime > 0 and enemyTextBox.getStr() == parryStr:
+            if parryTime > 0 and enemyTextBox.getMainStr() == parryStr:
                 parrySuccess = True
                 parryTextTime = 1
             if parryTime == 0: parryTime = 1
-            enemyTextBox.setStr('') # 이거 뒤에도 조금 남아있어야 패링이 될듯
+            enemyTextBox.setMainStr('')  # 이거 뒤에도 조금 남아있어야 패링이 될듯
 
         screen.fill(pygame.Color("black"))
         parryText = myFont.render("PARRY!!", True, (255, 255, 255))
         prQ = myFont.render(parryStr, True, (255, 255, 255))
 
-        myTextBox.drawBox(screen,(300,350))
-        enemyTextBox.drawBox(screen,(350,150))
+        myTextBox.drawBox(screen, (300, 350))
+        enemyTextBox.drawBox(screen, (350, 150))
 
         if parryTime > 0:
-            #screen.blit(prQ, (200, 150))
+            # screen.blit(prQ, (200, 150))
             parryTime += 1
         if parryTime > parryLast:
             parryTime = 0
@@ -159,14 +164,12 @@ def game(): ##페이커는 엄마가 없다.
         pygame.display.update()
 
         if global_t - enemyOffset >= enemyTypeInterval:
-            if enemyTextBox.getStr() == enemyAtk[currIdx]:
+            if enemyTextBox.getMainStr() == enemyAtk[currIdx]:
                 atkSet = False
-            enemyTextBox.setStr(enemyAtk[currIdx][:enemyTextBox.getLen() + 1])
+            enemyTextBox.setMainStr(enemyAtk[currIdx][:enemyTextBox.getMainLen() + 1])
             enemyOffset += enemyTypeInterval
 
-
         global_t += 1
-
 
 
 if __name__ == "__main__":
--- src/textbox.py
+++ src/textbox.py
@@ -1,37 +1,72 @@
 import pygame
 
+
 class textBox:
-    mainStr = ''
-    #frontStr = ''
-    color = (255,255,255)
     whiteBar = False
-    def __init__(self, f, s='', fs='', c=(255,255,255)) -> None:
+
+    def __init__(self, f, ml=20, c=(255, 255, 255), sc=(255, 255, 0), ac=(0, 255, 255), rc=(255, 255, 255)) -> None:
         self.font = f
-        self.mainStr = s
-        #self.frontStr =fs
-        self.color = c
+        self.maxLength = ml  # 입력창의 길이
 
-    def setStr(self,s):
+        self.mainStr = ''  # 입력 중인 텍스트
+        self.stunStr = ''  # 스턴 텍스트
+
+        self.mainColor = c  # 입력 텍스트 색깔 / 디폴트 : 하얀색
+        self.stunColor = sc  # 스턴 텍스트 색깔 / 디폴트 : 노란색
+        self.actionColor = ac  # 명령어 완성 시 텍스트 색깔 / 디폴트 : 하늘색
+        self.rectColor = rc  # 입력창 박스 색깔 / 디폴트 : 하얀색
+
+    def getRemainingLen(self):  # 입력창의 여유 공간을 반환함
+        return self.maxLength - len(self.mainStr) - len(self.stunStr)
+
+    def setMaxLength(self, i):
+        self.maxLength = i
+
+    # MainStr 관련 메서드
+
+    def setMainStr(self, s):
         self.mainStr = s
 
-    def getStr(self):
+    def getMainStr(self):
         return self.mainStr
 
-    def getLen(self):
+    def getMainLen(self):
         return len(self.mainStr)
-    
-    def setColor(self,c):
-        self.color = c
 
-    def subStr(self,i):
-        self.mainStr = self.mainStr[:len(self.mainStr)-i]
+    def addMainStr(self, s):
+        self.mainStr += s[0:self.getRemainingLen()]
 
-    def addStr(self,s):
-        self.mainStr += s
+    def subMainStrFromLeft(self, i):  # 가장 왼쪽 i개의 문자를 제거함
+        self.mainStr = self.mainStr[min(i, len(self.mainStr)):]
 
+    def subMainStrFromRight(self, i):  # 가장 오른쪽 i개의 문자를 제거함
+        self.mainStr = self.mainStr[:max(0, len(self.mainStr) - i)]
 
-    def drawBox(self,screen,pos):
-        text = self.font.render(self.mainStr, True, self.color)
-        pygame.draw.rect(screen, self.color, [pos[0],pos[1],300,self.font.size("a")[1]],4)
-        screen.blit(text, pos)
-    
+    # StunStr 관련 메서드
+
+    def setStunStr(self, s):
+        self.stunStr = s
+
+    def getStunStr(self):
+        return self.stunStr
+
+    def addStunStr(self, s):
+        self.stunStr += s[0:self.getRemainingLen()]
+
+    def subStunStrFromLeft(self, i):  # 가장 왼쪽 i개의 문자를 제거함
+        self.stunStr = self.mainStr[min(i, len(self.stunStr)):]
+
+    def subStunStrFromRight(self, i):  # 가장 오른쪽 i개의 문자를 제거함
+        self.stunStr = self.stunStr[:max(0, len(self.stunStr) - i)]
+
+    # 색 관련 메서드
+
+    def setColor(self, c):
+        self.mainColor = c
+
+    def drawBox(self, screen, pos):
+        allText = self.font.render(self.mainStr + ' ' * self.getRemainingLen() + self.stunStr, True, self.stunColor)
+        mainText = self.font.render(self.mainStr, True, self.mainColor)
+        pygame.draw.rect(screen, self.rectColor, [pos[0], pos[1], 300, self.font.size("a")[1]], 4)
+        screen.blit(allText, pos)
+        screen.blit(mainText, pos)
